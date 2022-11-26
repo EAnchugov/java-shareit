db63a1c75ebd9d6539de8accfb1b006037657949
EAnchugov <bergy@yandex.ru>
2022-11-26 14:17:24 +0300

    Comment data fix

--- src/main/java/ru/practicum/shareit/comment/model/Comment.java
+++ src/main/java/ru/practicum/shareit/comment/model/Comment.java
@@ -1,16 +1,14 @@
 package ru.practicum.shareit.comment.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 @Entity
-@Data
+@Getter
+@Setter
 @Table(name = "comments")
 @Builder
 @AllArgsConstructor

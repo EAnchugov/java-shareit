19524367fe0cf819880e4cd4648e1e93c75ce627
EAnchugov <bergy@yandex.ru>
2023-01-18 19:17:21 +0300

    UserService fix

--- src/main/java/ru/practicum/shareit/user/service/UserServiceJPA.java
+++ src/main/java/ru/practicum/shareit/user/service/UserServiceJPA.java
@@ -3,16 +3,14 @@ package ru.practicum.shareit.user.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ru.practicum.shareit.exceptions.DuplicateEmailException;
 import ru.practicum.shareit.exceptions.NotFoundException;
 import ru.practicum.shareit.user.UserMapper;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.repository.UserRepository;
 import ru.practicum.shareit.user.userDTO.UserDto;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -22,11 +20,7 @@ public class UserServiceJPA implements UserService {
 
     @Override
     public List<UserDto> getAll() {
-        List<UserDto> users = new ArrayList<>();
-        for (User user : userRepository.findAll()) {
-            users.add(UserMapper.toUserDTO(user));
-        }
-        return users;
+        return userRepository.findAll().stream().map(UserMapper::toUserDTO).collect(Collectors.toList());
     }
 
     @Override
@@ -46,30 +40,19 @@ public class UserServiceJPA implements UserService {
     @Transactional
     @Override
     public UserDto update(UserDto userdto, Long id) {
-        User user = UserMapper.toUser(userdto);
-        User user1 = UserMapper.toUser(getById(id));
-        if (!(Objects.equals(user.getEmail(), user1.getEmail()))) {
-            userDuplicateEmailCheck(user);
+        User updateUser = userRepository.findById(id).orElseThrow(() -> new NotFoundException("Пользователь не найден"));
+        if (userdto.getName() != null && !userdto.getName().isBlank()) {
+            updateUser.setName(userdto.getName());
         }
-        if (user.getName() != null && !user.getName().isBlank()) {
-            user1.setName(user.getName());
+        if (userdto.getEmail() != null && !(userdto.getEmail().isBlank())) {
+            updateUser.setEmail(userdto.getEmail());
         }
-        if (user.getEmail() != null) {
-            user1.setEmail(user.getEmail());
-        }
-        user1.setId(id);
-        return UserMapper.toUserDTO(userRepository.save(user1));
+        return UserMapper.toUserDTO(updateUser);
     }
 
     @Override
     @Transactional
     public void delete(Long id) {
         userRepository.deleteById(id);
-    }
-
-    private void userDuplicateEmailCheck(User user) {
-        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
-            throw new DuplicateEmailException("Email Duplicate");
-        }
     }
 }

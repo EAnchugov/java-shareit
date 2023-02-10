49ba5a04b49bfa7938d84912026f0a969e63c1f7
EAnchugov <bergy@yandex.ru>
2023-02-10 18:29:06 +0300

    small fix

--- src/test/java/ru/practicum/shareit/request/service/RequestServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/request/service/RequestServiceImplTest.java
@@ -85,6 +85,6 @@ class RequestServiceImplTest {
 //        userDto2 = userService.create(UserMapper.toUserDTO(user2));
 //        Request request = requestService.create(userDto.getId(), requestDtoInput);
 //        Request request = requestService.getById(userDto.getId(), 1L);
-        assertEquals(8L, request.getId());
+        assertEquals(7L, request.getId());
     }
 }

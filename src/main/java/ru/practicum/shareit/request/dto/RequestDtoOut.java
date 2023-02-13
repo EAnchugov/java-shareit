f361db134d1c767772731fa9960da994d9379326
EAnchugov <bergy@yandex.ru>
2023-02-13 17:47:35 +0300

    fix6

--- src/main/java/ru/practicum/shareit/request/dto/RequestDtoOut.java
+++ src/main/java/ru/practicum/shareit/request/dto/RequestDtoOut.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.request.dto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.extern.slf4j.Slf4j;
 import ru.practicum.shareit.item.itemDto.ItemDto;
 import ru.practicum.shareit.request.model.RequestAuthor;
 
@@ -12,7 +11,6 @@ import java.util.List;
 
 @Getter
 @Setter
-@Slf4j
 @Builder
 public class RequestDtoOut {
     private Long id;

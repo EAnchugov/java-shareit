02c6d91da170172dff3dc820593d9f4b3f351de2
EAnchugov <bergy@yandex.ru>
2023-02-20 15:04:18 +0300

    Usedto fix

---
+++ gateway/src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
@@ -0,0 +1,74 @@
+package ru.practicum.shareit.exceptions;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolationException;
+
+@Slf4j
+@RestControllerAdvice
+public class ErrorHandler {
+//
+//    @ExceptionHandler
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    private ErrorResponse handle(final WrongParameterException e) {
+//        log.error("Ошибка  " + e.getMessage());
+//        return new ErrorResponse(
+//                e.getMessage()
+//        );
+//    }
+//
+//    @ExceptionHandler
+//    @ResponseStatus(HttpStatus.NOT_FOUND)
+//    private ErrorResponse handle(final NotFoundException e) {
+//        log.error("Ошибка NotFoundException " + e.getMessage());
+//        return new ErrorResponse(
+//                e.getMessage()
+//        );
+//    }
+//
+//    @ExceptionHandler
+//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+//    private ErrorResponse handle(final Throwable e) {
+//        log.error("Ошибка Throwable e " + e.getMessage());
+//        return new ErrorResponse(
+//                e.getMessage()
+//        );
+//    }
+//
+//    @ExceptionHandler
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    private void handle(final MethodArgumentNotValidException e) {
+//        log.error("Ошибка MethodArgumentNotValidException " + e.getMessage());
+//    }
+//
+//    @ExceptionHandler
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    private ErrorResponse handle(final ItemNotAvailableException e) {
+//        log.error("Ошибка ItemNotAvailableException " + e.getMessage());
+//        return new ErrorResponse(
+//                e.getMessage());
+//    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> exc(Throwable ex) {
+        log.info("Код ошибки: 500");
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> exc(MethodArgumentNotValidException ex) {
+        log.info("Код ошибки 400");
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> exc(ConstraintViolationException ex) {
+        log.info("Код ошибки 500");
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
